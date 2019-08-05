bd4ee829eb441c74b449b4f668079c8f6000a6cb
HainSong <song_yz@buaa.edu.cn>
2019-08-05 12:17:17 +0800

    123

---
+++ 排序/选择排序.js
@@ -0,0 +1,25 @@
+/**
+ * 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，
+ * 然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ * 以此类推，直到所有元素均排序完毕。
+ */
+function selectionSort(arr) {
+    let len = arr.length;
+    let minIndex, temp;
+    console.time('选择排序耗时');
+    for (let i = 0; i < len - 1; i++) {
+        minIndex = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {     //寻找最小的数
+                minIndex = j;                 //将最小数的索引保存
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp; // 将最小数移动至当前位置
+    }
+    console.timeEnd('选择排序耗时');
+    return arr;
+}
+let arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
+console.log(selectionSort(arr));//[2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]
